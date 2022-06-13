7140a4a6e66daf7429654d7614b921e3309fb31c
Bárbara Cristina Monteiro Gurjão <102538457+barbaracmgurjao@users.noreply.github.com>
2022-06-13 15:01:35 -0300

    Create ex-3.js

--- parte-2/ex-3.js
+++ parte-2/ex-3.js
@@ -1,4 +1,4 @@
-// Escreva um algoritmo que recebe um array de nomes quaisquer e o nome da pessoa convida a ser encontrada.
+// Escreva um algoritmo que recebe um array de nomes quaisquer e o nome da pessoa convidada a ser encontrada.
 
 //  Se encontrar o nome na lista retorne:
 //  -----------------------
